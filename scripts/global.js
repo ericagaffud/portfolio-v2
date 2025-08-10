808e6e25c881f784dfbc9797e9053be14f7c213c
Erica Gaffud <ericadurangaffud@gmail.com>
2025-08-10 20:00:33 +0800

    components

---
+++ scripts/global.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('.header').load('./components/header.html');
+    $('.project-div').load('./components/project.html');
+    $('.about-div').load('./components/about.html');
+    $('.contact-div').load('./components/contact.html');
+});
